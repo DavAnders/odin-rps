636cb4fcfc7a89e5dfbdea94b86e9b99306511c5
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-03-30 21:28:47 -0500

    Add playRound basic functionality
    	Add playRound basic functionality

--- rps.js
+++ rps.js
@@ -1,9 +1,20 @@
-const options = ["rock", "paper", "scissors"];
+const options = ["Rock", "Paper", "Scissors"];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
 function getComputerChoice() {
   const choice = options[getRandomInt(3)];
   return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  let playerStringLower = playerSelection.toLowerCase();
+  let playerStringNorm = playerStringLower.charAt(0).toUpperCase() + playerStringLower.slice(1);
+  if (playerStringNorm === "Rock") {
+    if (computerSelection === "Rock") {
+      return `It's a tie! ${playerStringNorm} matches ${computerSelection}`
+    }
+  }
 }
