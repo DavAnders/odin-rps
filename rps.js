f46050d282ae38f74261880ad9131d72a7c8253e
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-03-30 22:15:56 -0500

    Add playGame function

--- rps.js
+++ rps.js
@@ -11,40 +11,69 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   let playerStringLower = playerSelection.toLowerCase();
-  let playerStringNorm = playerStringLower.charAt(0).toUpperCase() + playerStringLower.slice(1);
-  const winMsg = `You win! ${playerStringNorm} beats ${computerSelection}`
-  const loseMsg = `You lose! ${computerSelection} beats ${playerStringNorm}`
-  const tieMsg = `It's a tie! ${playerStringNorm} matches ${computerSelection}`
+  let playerStringNorm =
+    playerStringLower.charAt(0).toUpperCase() + playerStringLower.slice(1);
+  const winMsg = `You win! ${playerStringNorm} beats ${computerSelection}`;
+  const loseMsg = `You lose! ${computerSelection} beats ${playerStringNorm}`;
+  const tieMsg = `It's a tie! ${playerStringNorm} matches ${computerSelection}`;
 
   if (playerStringNorm === "Rock") {
     if (computerSelection === "Rock") {
-      return tieMsg
+      return tieMsg;
     } else if (computerSelection === "Paper") {
-      return loseMsg
+      return loseMsg;
     } else if (computerSelection === "Scissors") {
-      return winMsg
+      return winMsg;
     } else {
-      return "Unexpected Error"
+      return "Unexpected Error";
     }
   } else if (playerStringNorm === "Paper") {
     if (computerSelection === "Rock") {
-      return winMsg
+      return winMsg;
     } else if (computerSelection === "Paper") {
-      return tieMsg
+      return tieMsg;
     } else if (computerSelection === "Scissors") {
-      return loseMsg
+      return loseMsg;
     } else {
-      return "Unexpected Error"
+      return "Unexpected Error";
     }
   } else if (playerStringNorm === "Scissors") {
     if (computerSelection === "Rock") {
-      return loseMsg
+      return loseMsg;
     } else if (computerSelection === "Paper") {
-      return winMsg
+      return winMsg;
     } else if (computerSelection === "Scissors") {
-      return tieMsg
+      return tieMsg;
     } else {
-      return "Unexpected Error"
+      return "Unexpected Error";
     }
+  } else {
+    return "Unexpected Error: invalid user input";
   }
+}
+
+function winCheck(result) {
+  if (result.includes("win")) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function playGame() {
+  let score = 0;
+  let rounds = 0;
+  // 5 rounds baseline
+  for (let i = 0; i < 5; i++) {
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Choose Rock, Paper, or Scissors");
+    let result = playRound(playerSelection, computerSelection);
+    console.log(result);
+    score += winCheck(result);
+    rounds += 1;
+  }
+  if (score === 1) {
+    return `Your final score is ${score} win after ${rounds} rounds.`;
+  }
+  return `Your final score is ${score} wins after ${rounds} rounds.`;
 }
