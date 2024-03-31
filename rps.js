e9edd6fcc6d48ebc911fccfeabcc89319b1460c0
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-03-30 21:45:16 -0500

    Add playRound function

--- rps.js
+++ rps.js
@@ -12,9 +12,39 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   let playerStringLower = playerSelection.toLowerCase();
   let playerStringNorm = playerStringLower.charAt(0).toUpperCase() + playerStringLower.slice(1);
+  const winMsg = `You win! ${playerStringNorm} beats ${computerSelection}`
+  const loseMsg = `You lose! ${computerSelection} beats ${playerStringNorm}`
+  const tieMsg = `It's a tie! ${playerStringNorm} matches ${computerSelection}`
+
   if (playerStringNorm === "Rock") {
     if (computerSelection === "Rock") {
-      return `It's a tie! ${playerStringNorm} matches ${computerSelection}`
+      return tieMsg
+    } else if (computerSelection === "Paper") {
+      return loseMsg
+    } else if (computerSelection === "Scissors") {
+      return winMsg
+    } else {
+      return "Unexpected Error"
+    }
+  } else if (playerStringNorm === "Paper") {
+    if (computerSelection === "Rock") {
+      return winMsg
+    } else if (computerSelection === "Paper") {
+      return tieMsg
+    } else if (computerSelection === "Scissors") {
+      return loseMsg
+    } else {
+      return "Unexpected Error"
+    }
+  } else if (playerStringNorm === "Scissors") {
+    if (computerSelection === "Rock") {
+      return loseMsg
+    } else if (computerSelection === "Paper") {
+      return winMsg
+    } else if (computerSelection === "Scissors") {
+      return tieMsg
+    } else {
+      return "Unexpected Error"
     }
   }
 }
